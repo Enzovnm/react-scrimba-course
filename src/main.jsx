016c0e66d4ec12a245c84647fe20336076a92d3f
Enzo Monteiro <enzovila.monteiro@gmail.com>
2025-06-07 15:08:30 -0300

    feat: Challenge using vanilla js to compare declarative & imperative programming

--- src/main.jsx
+++ src/main.jsx
@@ -5,4 +5,14 @@ const MainContent = () => {
   return <h1>React is great!</h1>;
 };
 
+const text = document.createElement("h1");
+
+text.textContent = "Texto de teste";
+
+text.className = "header";
+
+const divRoot = document.getElementById("root");
+
+divRoot.appendChild(text);
+
 createRoot(document.getElementById("root")).render(<MainContent />);
