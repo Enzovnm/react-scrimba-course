ed6902f7b08af70f296d58f6304e16c8451f7459
Enzo Monteiro <enzovila.monteiro@gmail.com>
2025-06-06 23:40:24 -0300

    feat: Nesting createElement

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { createRoot } from "react-dom/client";
 import { createElement } from "react";
 import "./index.css";
 
+const reactElement = createElement("h1", null, createElement("span", null, "I'm inside span"));
+
+console.log(reactElement)
 createRoot(document.getElementById("root")).render(
-  createElement("h1", null, "Hello from createElement!")
+  reactElement
 );
