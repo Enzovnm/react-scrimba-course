16843e8674118b16afb67be9c926950fdcebd499
Enzo Monteiro <enzovila.monteiro@gmail.com>
2025-06-07 15:15:34 -0300

    feat: Challenge - Creating a react from strach

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,7 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
 
-const MainContent = () => {
-  return <h1>React is great!</h1>;
-};
+const root = createRoot(document.getElementById("root"));
 
-const text = document.createElement("h1");
-
-text.textContent = "Texto de teste";
-
-text.className = "header";
-
-const divRoot = document.getElementById("root");
-
-divRoot.appendChild(text);
-
-createRoot(document.getElementById("root")).render(<MainContent />);
+root.render(
+  <h1>Hellow word</h1>
+);
