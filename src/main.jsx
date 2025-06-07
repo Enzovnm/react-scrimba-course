2f39c2f43701abcbded9ab77a875de781886c18f
Enzo Monteiro <enzovila.monteiro@gmail.com>
2025-06-06 22:57:06 -0300

    Learning using createElement function

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
-
+import { createElement } from "react";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render();
+createRoot(document.getElementById("root")).render(
+  createElement("h1", null, "Hello from createElement!")
+);
